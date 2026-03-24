7b6c2465a723403957dd66d74154637b1ff71e41
professor <pift3345@blr08.duckietown.local>
2026-03-24 11:32:03 -0400

    fixing RViZ topic bug

--- assets/vnc/usr/bin/launch-ekf-exercise.sh
+++ assets/vnc/usr/bin/launch-ekf-exercise.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s/agent/${VEHICLE_NAME}/g" /opt/ros/noetic/share/rviz/ekf-localization.rviz
+sed -i "s/vlx/${VEHICLE_NAME}/g" /opt/ros/noetic/share/rviz/ekf-localization.rviz
 rviz -d /opt/ros/noetic/share/rviz/ekf-localization.rviz
